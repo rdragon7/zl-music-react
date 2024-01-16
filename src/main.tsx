7f51f51c66996e6e102af8670c55f3f644d7d3ac
coder7 <ren760824@163.com>
2024-01-16 15:39:20 +0800

    fix(retry): project retry

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
+// import './style/base.less'
+import '@/style/base.less'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import eslintPlugin from 'vite-plugin-eslint'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslintPlugin()]
+  plugins: [react(), eslintPlugin()],
+  // 配置别名
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
